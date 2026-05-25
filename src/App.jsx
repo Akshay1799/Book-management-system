cadc240f5634c61bdb35b71274e110e5b8e325d4
Akshay Ladne <Kumawata283@gmail.com>
2026-05-25 20:05:46 +0530

    Setup the project, configure tailwindcss for styling

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold flex justify-center items-center h-screen">Book Management System</h1>
+    </>
+  )
+}
+
+export default App
